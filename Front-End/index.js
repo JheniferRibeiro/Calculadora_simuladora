dc1ab301d379832c931df3b7f503014529d7dd1c
JheniferRibeiro <jheniferribeirolopes_@hotmail.com>
2023-03-14 20:11:16 -0300

    Criação das funções.

--- Front-End/index.js
+++ Front-End/index.js
@@ -4,18 +4,33 @@ let valorMensal = 0;
 let taxaJuros = 0;
 let qtdMeses = 0;
 let resultado = 0;
+let resultadoTotalInvestido = 0;
+let resultadoTotalJuros = 0;
 
+// Função para pegar os dados do html
 function getValueElement(id) {
-    var elemento = document.getElementById("valor-inicial");
+    var elemento = document.getElementById(id);
     return elemento.value;
 }
 
-// Função para pegar os dados do 
+// Pegar os dados 
 function getDados() {
     valorInicial = getValueElement("valor-inicial");
     valorMensal = getValueElement("valor-mensal");
     taxaJuros = getValueElement("taxa-juros");
-    qtdMeses = getValueElement("periodo");                
+    qtdMeses = getValueElement("periodo");        
+    
+    resultado = valorInicial * Math.pow( (1+taxaJuros/100), qtdMeses/12);
+    var totalFinalElemento = document.getElementById("total-final");
+    totalFinalElemento.innerText = resultado.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+    resultadoTotalInvestido = parseFloat(valorInicial);
+    var totalInvestidoElemento = document.getElementById("total-investido");
+    totalInvestidoElemento.innerText = resultadoTotalInvestido.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    
+    resultadoTotalJuros = resultado - valorInicial;
+    var totalJurosElemento = document.getElementById("total-juros");
+    totalJurosElemento.innerText = resultadoTotalJuros.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }
 
 // Função
